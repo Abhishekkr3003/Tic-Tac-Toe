105966e3a7bf02c19f83bcd0a3782c1fce23f7c2
Abhishek Kumar <abhishek@MMMDVABHISHEKKUINT.local>
2023-07-18 19:31:10 +0530

    adds game class

---
+++ src/main/java/org/example/Game.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Game {
+    Boolean turn;
+    final Integer gameId;
+
+    public Game(Integer gameId) {
+        this.gameId = gameId;
+        turn = false;
+    }
+
+    void makeMove() {
+
+    }
+
+    void checkWinner() {
+
+    }
+
+    void playGame() {
+
+    }
+}
